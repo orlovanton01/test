84f6af2c2e3a5f64eee255aebf9d1193c1b54faa
orlovanton01 <aov-perm@mail.ru>
2024-09-08 09:07:51 +0500

    Initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    private TcpClient client;
+    private NetworkStream stream;
+
+    public Client()
+    {
+        client = new TcpClient("127.0.0.1", 8888);
+        stream = client.GetStream();
+    }
+
+    public void Start()
+    {
+        Thread receivingThread = new Thread(ReceiveData);
+        receivingThread.Start();
+    }
+
+    private void ReceiveData()
+    {
+        byte[] buffer = new byte[256];
+        while (true)
+        {
+            try
+            {
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                if (bytesRead == 0)
+                {
+                    Console.WriteLine("Соединение с сервером потеряно");
+                    break;
+                }
+
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine(message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+                break;
+            }
+        }
+        client.Close();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Client client = new Client();
+        client.Start();
+        Console.ReadLine(); // ожидание завершения работы клиента
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Server
+{
+    private TcpListener listener;
+
+    public Server()
+    {
+        listener = new TcpListener(IPAddress.Any, 8888);
+        listener.Start();
+        Console.WriteLine("Сервер запущен и ожидает подключения...");
+    }
+
+    public void Start()
+    {
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Клиент подключен");
+            Thread clientThread = new Thread(() => HandleClient(client));
+            clientThread.Start();
+        }
+    }
+
+    private void HandleClient(TcpClient client)
+    {
+        NetworkStream stream = client.GetStream();
+        while (true)
+        {
+            try
+            {
+                DateTime thisDay = DateTime.Now;
+                string stringDay = thisDay.ToString();
+                int even = 0; // чётные
+                int odd = 0; // нечетные
+                string message;
+                foreach (var item in stringDay)
+                    try
+                    {
+                        int value = int.Parse(Convert.ToString(item));
+                        if (value % 2 == 0)
+                            even++;
+                        else
+                            odd++;
+                    }
+                    catch
+                    { }
+                if (even > odd)
+                    message = "чет!";
+                else if (even < odd)
+                    message = "нечет!";
+                else
+                    message = "равно!";
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Отправлено: " + message);
+                Thread.Sleep(1000);  // пауза перед следующей отправкой
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+                break;
+            }
+        }
+        client.Close();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Server server = new Server();
+        server.Start();
+    }
+}
